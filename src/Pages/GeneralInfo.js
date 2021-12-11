ace5d14d156329219b09902ffc7361f5f95c2855
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-11 07:32:58 +0200

    Updated general info to be more dynamic

--- src/Pages/GeneralInfo.js
+++ src/Pages/GeneralInfo.js
@@ -1,144 +1,303 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Chart from "react-apexcharts";
-import Cookies from 'universal-cookie';
-import StatItem from '../Components/StatItem';
-import StatChart from '../Components/StatChart';
-import { Container, Row, Col } from 'react-bootstrap';
-
+import Cookies from "universal-cookie";
+import StatItem from "../Components/StatItem";
+import StatChart from "../Components/StatChart";
+import { Container, Row, Col } from "react-bootstrap";
+import Lottie from "lottie-react";
+import loading from "./../Assets/animations/loading.json";
 
 export class GeneralInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: true,
-            usersChartOptions: null,
-            usersChartSeries: null,
-            applicationsChartOptions: null,
-            applicationsChartSeries: null,
-            numberOfActiveUsers: null,
-            numberOfWebUsers: null,
-            numberOfMobileUsers: null,
-            numberOfAhedActiveUsers: null,
-            numberOfAtaaActiveUsers: null,
-            numberOfTimeCatcherActiveUsers: null,
-            numberOfMemoryWallActiveUsers: null,
-        };
-    }
-    componentDidMount() {
-        const axios = require('axios').default;
-        const cookies = new Cookies();
-        cookies.set('accessToken', 'v20H0h0VQ8xxNoarrsGx5CxFJw8kzwN1vlJkftZvSdwcnC5tsjWhEgOcRFK9', { path: '/' });
-        console.log(cookies.get('accessToken'));
-        axios.get('http://192.168.1.139:8000/api/admin?userId=1',
-            {
-                headers:
-                {
-                    'Authorization': 'Bearer ' + cookies.get('accessToken')
-                }
-            }
-        ).then((response) => {
-            let data = response.data;
-            if (data['Err_Flag']) {
-                return this.setState({
-                    isLoading: false,
-                    error: data['Err_Desc']
-                });
-            }
-            this.setState({
-                isLoading: false,
-                numberOfActiveUsers: data['data']['General']['NumberOfActiveUsers'],
-                numberOfWebUsers: data['data']['General']['NumberOfWebUsers'],
-                numberOfMobileUsers: data['data']['General']['NumberOfMobileUsers'],
-                numberOfAhedActiveUsers: data['data']['General']['NumberOfAhedActiveUsers'],
-                numberOfAtaaActiveUsers: data['data']['General']['NumberOfAtaaActiveUsers'],
-                numberOfTimeCatcherActiveUsers: data['data']['General']['NumberOfTimeCatcherActiveUsers'],
-                numberOfMemoryWallActiveUsers: data['data']['General']['NumberOfMemoryWallActiveUsers'],
-                usersChartSeries: [data['data']['General']['NumberOfWebUsers'], data['data']['General']['NumberOfMobileUsers']],
-                usersChartOptions: {
-                    chart: {
-                        type: 'polarArea',
-                    },
-                    labels: ['Number of Web Users', 'Number of Mobile Users'],
-                    stroke: {
-                        colors: ['#fff']
-                    },
-                    fill: {
-                        opacity: 0.8
-                    },
-                    responsive: [{
-                        breakpoint: undefined,
-                    }]
-                },
-                applicationsChartSeries: [
-                    data['data']['General']['NumberOfAhedActiveUsers'],
-                    data['data']['General']['NumberOfAtaaActiveUsers'],
-                    data['data']['General']['NumberOfTimeCatcherActiveUsers'],
-                    data['data']['General']['NumberOfMemoryWallActiveUsers'],
-                ],
-                applicationsChartOptions: {
-                    chart: {
-                        type: 'polarArea',
-                    },
-                    labels: ['Number of Ahed Active Users', 'Number of Ataa Active Users', 'Number of Time Catcher Active Users', 'Number of Memory Wall Active Users',],
-                    stroke: {
-                        colors: ['#fff']
-                    },
-                    fill: {
-                        opacity: 0.8
-                    },
-                    responsive: [{
-                        breakpoint: undefined,
-                    }]
-                },
-            });
-        })
-            .catch((error) => {
-                console.log(error);
-                this.setState({
-                    isLoading: false,
-                    error
-                });
-            });
-    }
-    render() {
-        const { error, isLoading, items } = this.state;
-        console.log(this.state.isLoading);
-        if (error) {
-            return <div>Error: {error.message}</div>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      usersChartOptions: null,
+      usersChartSeries: null,
+      applicationsChartOptions: null,
+      applicationsChartSeries: null,
+      numberOfJoinedUsersGroupedByYearOptions: null,
+      numberOfJoinedUsersGroupedByYearSeries: null,
+      numberOfUsersGroupedByNationalityOptions: null,
+      numberOfUsersGroupedByNationalitySeries: null,
+      numberOfActiveUsers: null,
+      numberOfActiveUsersGroupedByAccessType: null,
+      numberOfActiveUsersGroupedByAppType: null,
+    };
+  }
+  componentDidMount() {
+    const axios = require("axios").default;
+    const cookies = new Cookies();
+    cookies.set(
+      "accessToken",
+      "QAIWgfuD0umt7XIBinn4YpnsUgaWdjHOiHT9E4DV1ZAG8WVhBNvUSQDMuRCL",
+      { path: "/" }
+    );
+    axios
+      .get("http://192.168.1.3:8000/api/admin?userId=1", {
+        headers: {
+          Authorization: "Bearer " + cookies.get("accessToken"),
+        },
+      })
+      .then((response) => {
+        let data = response.data;
+        if (data["Err_Flag"]) {
+          return this.setState({
+            isLoading: false,
+            error: data["Err_Desc"],
+          });
         }
-        return (
-            <Container>
-                <Row>
-                    <Col xs={6} md={4}><StatItem isLoading={this.state.isLoading} number={this.state.numberOfWebUsers} text={'Number of Web Users'} /></Col>
-                    <Col xs={6} md={4}><StatItem isLoading={this.state.isLoading} number={this.state.numberOfActiveUsers} text={'Number of Active Users'} /></Col>
-                    <Col xs={6} md={4}><StatItem isLoading={this.state.isLoading} number={this.state.numberOfMobileUsers} text={'Number of Mobile Users'} /></Col>
-                </Row>
+        this.setState({
+          isLoading: false,
+          numberOfActiveUsers: data["data"]["General"]["NumberOfActiveUsers"],
+          numberOfActiveUsersGroupedByAccessType:
+            data["data"]["General"]["NumberOfActiveUsersGroupedByAccessType"],
+          numberOfActiveUsersGroupedByAppType:
+            data["data"]["General"]["NumberOfActiveUsersGroupedByAppType"],
+          usersChartSeries: data["data"]["General"][
+            "NumberOfActiveUsersGroupedByAccessType"
+          ].map((data) => {
+            return data.count;
+          }),
+          usersChartOptions: {
+            chart: {
+              type: "polarArea",
+            },
+            labels: data["data"]["General"][
+              "NumberOfActiveUsersGroupedByAccessType"
+            ].map((data) => {
+              return data.accessType;
+            }),
+            stroke: {
+              colors: ["#fff"],
+            },
+            fill: {
+              opacity: 0.8,
+            },
+            responsive: [
+              {
+                breakpoint: undefined,
+              },
+            ],
+          },
+          applicationsChartSeries: data["data"]["General"][
+            "NumberOfActiveUsersGroupedByAppType"
+          ].map((data) => {
+            return data.count;
+          }),
+          applicationsChartOptions: {
+            chart: {
+              type: "polarArea",
+            },
+            labels: data["data"]["General"][
+              "NumberOfActiveUsersGroupedByAppType"
+            ].map((data) => {
+              return data.appType;
+            }),
+            stroke: {
+              colors: ["#fff"],
+            },
+            fill: {
+              opacity: 0.8,
+            },
+            responsive: [
+              {
+                breakpoint: undefined,
+              },
+            ],
+          },
+          numberOfJoinedUsersGroupedByYearSeries: [
+            {
+              data: data["data"]["General"]["NumberOfJoinedUsersByYear"].map(
+                (data) => {
+                  return data.count;
+                }
+              ),
+            },
+          ],
+          numberOfJoinedUsersGroupedByYearOptions: {
+            chart: {
+              height: 350,
+              type: "bar",
+              events: {
+                click: function (chart, w, e) {
+                  // console.log(chart, w, e)
+                },
+              },
+            },
+            plotOptions: {
+              bar: {
+                columnWidth: "45%",
+                distributed: true,
+              },
+            },
+            dataLabels: {
+              enabled: false,
+            },
+            legend: {
+              show: false,
+            },
+            xaxis: {
+              categories: data["data"]["General"][
+                "NumberOfJoinedUsersByYear"
+              ].map((data) => {
+                return data.year;
+              }),
+              labels: {
+                data: ["1"],
+                style: {
+                  fontSize: "12px",
+                },
+              },
 
-                <Row>
-                    <Col xs={6} md={4}><StatItem isLoading={this.state.isLoading} number={this.state.numberOfAhedActiveUsers} text={'Number of Ahed Active Users'} /></Col>
-                    <Col xs={6} md={4}><StatItem isLoading={this.state.isLoading} number={this.state.numberOfAtaaActiveUsers} text={'Number of Ataa Active Users'} /></Col>
-                    <Col xs={6} md={4}><StatItem isLoading={this.state.isLoading} number={this.state.numberOfMemoryWallActiveUsers} text={'Number of Memory Wall Active Users'} /></Col>
-                    <Col xs={6} md={4}>
-                        <StatItem isLoading={this.state.isLoading} number={this.state.numberOfTimeCatcherActiveUsers} text={'Number of Time Catcher Active Users'} /></Col>
-                </Row>
-                <Row xs={1} md={2}>
-                    <Col>
-                        <StatChart isLoading={this.state.isLoading} options={this.state.usersChartOptions} series={this.state.usersChartSeries} type="polarArea" />
-                    </Col>
-                    <Col>
-                        <StatChart isLoading={this.state.isLoading} options={this.state.applicationsChartOptions} series={this.state.applicationsChartSeries} type="polarArea" />
-                    </Col>
-                </Row>
+              //  [
+              //   {
+              //     name: "series-1",
+              //     data: [30, 40, 45, 50, 49, 60, 70, 91]
+              //   }
+              // ]
+            },
+            labels: data["data"]["General"]["NumberOfJoinedUsersByYear"].map(
+              (data) => {
+                return data.year;
+              }
+            ),
+          },numberOfUsersGroupedByNationalitySeries: [
+            {
+              data: data["data"]["General"]["NumberOfUsersGroupedByNationality"].map(
+                (data) => {
+                  return data.count;
+                }
+              ),
+            },
+          ],
+          numberOfUsersGroupedByNationalityOptions: {
+            chart: {
+              height: 350,
+              type: "bar",
+              events: {
+                click: function (chart, w, e) {
+                  // console.log(chart, w, e)
+                },
+              },
+            },
+            plotOptions: {
+              bar: {
+                columnWidth: "45%",
+                distributed: true,
+              },
+            },
+            dataLabels: {
+              enabled: false,
+            },
+            legend: {
+              show: false,
+            },
+            xaxis: {
+              categories: data["data"]["General"][
+                "NumberOfUsersGroupedByNationality"
+              ].map((data) => {
+                return data.nationality;
+              }),
+              labels: {
+                data: ["1"],
+                style: {
+                  fontSize: "12px",
+                },
+              },
 
-                {/* <Chart
-                    options={this.state.options}
-                    series={this.state.series}
-                    type="bar"
-                    width="500"
-                /> */}
-            </Container>
-        );
-    }
+              //  [
+              //   {
+              //     name: "series-1",
+              //     data: [30, 40, 45, 50, 49, 60, 70, 91]
+              //   }
+              // ]
+            },
+            labels: data["data"]["General"]["NumberOfUsersGroupedByNationality"].map(
+              (data) => {
+                return data.nationality;
+              }
+            ),
+          },
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          isLoading: false,
+          error,
+        });
+      });
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <div className="text-warning">Error: {this.state.error.message}</div>
+      );
+    } else if (this.state.isLoading)
+      return (
+        <div className="text-center mx-auto" style={{ width: "10%" }}>
+          <Lottie animationData={loading} />
+        </div>
+      );
+    return (
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <StatItem
+              number={this.state.numberOfActiveUsers}
+              text={"Number of Active Users"}
+            />
+          </Col>
+          {this.state.numberOfActiveUsersGroupedByAccessType.map((data) => {
+            return (
+              <Col xs={6} md={4}>
+                <StatItem number={data.count} text={data.accessType} />
+              </Col>
+            );
+          })}
+        </Row>
+        <Row>
+          {this.state.numberOfActiveUsersGroupedByAppType.map((data) => {
+            return (
+              <Col xs={6} md={4}>
+                <StatItem number={data.count} text={data.appType} />
+              </Col>
+            );
+          })}
+        </Row>
+        <Row>
+          <Col xs={6} md={6}>
+            <StatChart
+              options={this.state.usersChartOptions}
+              series={this.state.usersChartSeries}
+              type="polarArea"
+            />
+          </Col>
+          <Col xs={6} md={6}>
+            <StatChart
+              options={this.state.applicationsChartOptions}
+              series={this.state.applicationsChartSeries}
+              type="polarArea"
+            />
+          </Col>
+          <Col xs={6} md={6}>
+            <StatChart
+              options={this.state.numberOfJoinedUsersGroupedByYearOptions}
+              series={this.state.numberOfJoinedUsersGroupedByYearSeries}
+              type="bar"
+            />
+          </Col>
+          <Col xs={6} md={6}>
+            <StatChart
+              options={this.state.numberOfUsersGroupedByNationalityOptions}
+              series={this.state.numberOfUsersGroupedByNationalitySeries}
+              type="bar"
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default GeneralInfo
+export default GeneralInfo;

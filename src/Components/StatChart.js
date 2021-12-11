c1d8b57b42f305552fbc793b349515b0c601baee
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2021-12-11 07:32:28 +0200

    Removing unused functions

--- src/Components/StatChart.js
+++ src/Components/StatChart.js
@@ -1,35 +1,8 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import ReactApexChart from 'react-apexcharts';
-import Lottie from "lottie-react";
-import loading from "./../Assets/animations/loading.json";
 
 export class StatChart extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { width: 0, height: 0 };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions() {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
-
-        console.log(this.state.width);
-    }
     render() {
-        if (this.props.isLoading)
-            return (
-                <div className="text-center mx-auto" style={{ width: this.state.height / 10 }} >
-                    <Lottie animationData={loading} />
-                </div>)
         return (
             <ReactApexChart options={this.props.options} series={this.props.series} type={this.props.type} />
         )
--- src/Components/StatItem.js
+++ src/Components/StatItem.js
@@ -1,40 +1,11 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Lottie from "lottie-react";
-import loading from "./../Assets/animations/loading.json";
 
 export class StatItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { width: 0, height: 0 };
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-    componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener('resize', this.updateWindowDimensions);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateWindowDimensions);
-    }
-
-    updateWindowDimensions() {
-        this.setState({ width: window.innerWidth, height: window.innerHeight });
-
-        console.log(this.state.width);
-    }
     render() {
-        if (this.props.isLoading)
-            return (<div className="text-center">
-                <div className="text-center mx-auto" style={{ width: this.state.height / 25 }} >
-                    <Lottie animationData={loading} />
-                </div>
-                <h4>{this.props.text}</h4>
-            </div>)
         return (
             <div>
-                <h1>{this.props.number}</h1>
-                <h4>{this.props.text}</h4>
+                <h1 className='text-white'>{this.props.number}</h1>
+                <h4 className='text-white'>{this.props.text}</h4>
             </div>
         )
     }
